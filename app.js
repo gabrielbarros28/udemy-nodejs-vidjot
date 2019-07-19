112821f235f3844658cf3e5a811a8d260f6cb8c1
gabriel <gabriel_lana28@hotmail.com>
2019-07-19 11:28:42 -0300

    Added Update and delete video ideas

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -16,7 +19,7 @@ mongoose.connect('mongodb://localhost/vidjot-dev',{
 require('./models/Idea');
 const Idea = mongoose.model('ideas');
 
-
+//MIDDLEWARES
 //handlebars middleware
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
@@ -27,6 +30,27 @@ app.set('view engine','handlebars');
 //body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//method override middleware
+app.use(methodOverride('_method'));
+
+//Session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//flash middleware
+app.use(flash());
+
+//GLOBAL variables
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //ROUTES
 //Index route
@@ -66,6 +90,20 @@ app.get('/ideas/add', (req, res)=>{
     res.render('ideas/add');
 });
 
+// edit idea form
+app.get('/ideas/edit/:id', (req, res)=>{
+
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea =>{
+        res.render('ideas/edit',{
+            idea: idea
+        });
+    });
+    
+});
+
 //Process form
 
 app.post('/ideas', (req, res)=>{
@@ -93,11 +131,42 @@ app.post('/ideas', (req, res)=>{
 
         new Idea(newUser).save()
         .then(idea => {
+            req.flash('success_msg', 'Video Idea added');
             res.redirect('/ideas');
         });
     }
 });
 
+//edit form process
+app.put('/ideas/:id',(req, res) =>{
+
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea =>{
+        //new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save()
+        .then(idea =>{
+            req.flash('success_msg', 'Video Idea updated');
+            res.redirect('/ideas')
+        });
+    })
+});
+
+//delete idea
+app.delete('/ideas/:id', (req, res)=>{
+
+    Idea.remove({
+        _id: req.params.id
+    })
+    .then(() => {
+        req.flash('success_msg', 'Video Idea removed');
+        res.redirect('/ideas');
+    });
+});
 
 const port = 5000;
 app.listen(port, ()=>{
